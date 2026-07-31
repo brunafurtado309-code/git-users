26f22f7bc2ef907301e5a63007ae85be317dd4ce
Brunpaixao-10 <brunafurtado309@gmail.com>
2026-07-30 21:40:55 -0300

    feat: adicionar estrutura de busca de usuarios a partir de entrada de busca no navegador

--- server/src/mocks/user.mock.js
+++ server/src/mocks/user.mock.js
@@ -1,12 +1,15 @@
 const users = [{
+    id: 1,
     name: 'Alex Bessa',
     age: 30,
     status: true
 }, {
+    id: 2,
     name: 'Valdiana Bessa',
     age: 31,
     status: true
 }, {
+    id: 3,
     name: 'Ana Bessa',
     age: 0,
     status: false
